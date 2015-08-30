2ed9926b1f8eec6bdf92ca9dcea5b1d9fc10bd57
Shawn Nock <nock@nocko.se>
2015-08-30 15:26:05 -0400

    Manual optimizations to shrink resultant gif size

--- keyer.sh
+++ keyer.sh
@@ -22,24 +22,24 @@ map=([a]=sl [b]=lsss [c]=lsls [d]=lss [e]=s [f]=ssls [g]=lls \
 	[t]=l [u]=ssl [v]=sssl [w]=sll [x]=lssl [y]=lsll \
 	[z]=llss)
 
-conv_args="-delay $long_delay $keyup_pic"
+conv_args="-dispose none -delay 0 $keyup_pic"
 for (( i=0; i<${#phrase}; i++ )); do
     char=${phrase:$i:1}
-    if [ 0 -eq ${#map[$char]} ]; then
-	conv_args="$conv_args ( -delay $word_delay -clone 0 ) "
+    if [ -z ${map[$char]} ]; then
+	conv_args="$conv_args ( -delay $word_delay placeholder.png ) "
     fi
     for (( j=0; j<${#map[$char]}; j++ )); do
 	symbol=${map[$char]:$j:1}
 	if [ ${symbol}x == "sx" ]; then
-	    conv_args="$conv_args ( -delay $short_delay $keydown_pic "
+	    conv_args="$conv_args ( -delay $short_delay -dispose previous $keydown_pic"
 	elif [ ${symbol}x == "lx" ]; then
-	    conv_args="$conv_args ( -delay $long_delay $keydown_pic "
+	    conv_args="$conv_args ( -delay $long_delay -dispose previous $keydown_pic"
 	fi
 	if [ ${j} == $(expr ${#map[$char]} - 1) ]; then
-	     conv_args="$conv_args -delay $long_delay -clone 0 )"
+	     conv_args="$conv_args -delay $long_delay placeholder.png )"
 	else
-	     conv_args="$conv_args -delay $short_delay -clone 0 )"
+	     conv_args="$conv_args -delay $short_delay placeholder.png )"
 	fi
     done
 done
-convert -loop 1 $conv_args -layers Optimize output.gif
+convert -loop 1 $conv_args output.gif
