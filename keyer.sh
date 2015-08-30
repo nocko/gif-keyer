a311e2463a1c6b0cfbf31f289953cee35c574097
Shawn Nock <nock@nocko.se>
2015-08-30 15:37:37 -0400

    Use . and - in assoc array... for reasons

--- keyer.sh
+++ keyer.sh
@@ -16,11 +16,11 @@ long_delay=$(( $short_delay*3 ))
 word_delay=$(( $short_delay*7 ))
 
 declare -A map
-map=([a]=sl [b]=lsss [c]=lsls [d]=lss [e]=s [f]=ssls [g]=lls \
-	[h]=ssss [i]=ss [j]=slll [k]=lsl [l]=slss [m]=ll \
-	[n]=ls [o]=lll [p]=slls [q]=llsl [r]=sls [s]=sss \
-	[t]=l [u]=ssl [v]=sssl [w]=sll [x]=lssl [y]=lsll \
-	[z]=llss)
+map=([a]=.- [b]=-... [c]=-.-. [d]=-.. [e]=. [f]=..-. [g]=--. \
+	[h]=.... [i]=.. [j]=.--- [k]=-.- [l]=.-.. [m]=-- \
+	[n]=-. [o]=--- [p]=.--. [q]=--.- [r]=.-. [s]=... \
+	[t]=- [u]=..- [v]=...- [w]=.-- [x]=-..- [y]=-.-- \
+	[z]=--..)
 
 conv_args="-dispose none -delay 0 $keyup_pic"
 for (( i=0; i<${#phrase}; i++ )); do
@@ -30,9 +30,9 @@ for (( i=0; i<${#phrase}; i++ )); do
     fi
     for (( j=0; j<${#map[$char]}; j++ )); do
 	symbol=${map[$char]:$j:1}
-	if [ ${symbol}x == "sx" ]; then
+	if [ ${symbol}x == ".x" ]; then
 	    conv_args="$conv_args ( -delay $short_delay -dispose previous $keydown_pic"
-	elif [ ${symbol}x == "lx" ]; then
+	elif [ ${symbol}x == "-x" ]; then
 	    conv_args="$conv_args ( -delay $long_delay -dispose previous $keydown_pic"
 	fi
 	if [ ${j} == $(expr ${#map[$char]} - 1) ]; then
